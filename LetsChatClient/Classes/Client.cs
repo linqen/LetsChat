3d5a5ceca79c525038017acb4b93f02d744d9e4f
Linqen <avena47@gmail.com>
2019-05-21 15:56:58 -0300

    Client sending "Hello World!" using a button.

---
+++ LetsChatClient/Classes/Client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LetsChatClient
+{
+
+    class Client
+    {
+        public const int BUFFERSIZE = 1024;
+        public const int MYPORT = 62222;
+        public const int SERVERPORT = 65444;
+        public const string SERVERIP = "127.0.0.1";
+
+        private UdpClient myUdpClient;
+
+        public Client()
+        {
+            
+        }
+        public bool Connect()
+        {
+            myUdpClient = new UdpClient(MYPORT);
+
+            return true;
+        }
+        public bool Disconnect()
+        {
+
+            return true;
+        }
+        public void SendMessage()
+        {
+            string message = "Hello World!";
+            byte[] bytedMessage = Encoding.UTF8.GetBytes(message);
+            IPAddress ipAddress;
+            IPAddress.TryParse(SERVERIP, out ipAddress);
+            IPEndPoint endpoint = new IPEndPoint(ipAddress, SERVERPORT);
+            myUdpClient.Send(bytedMessage, bytedMessage.Length, endpoint);
+        }
+        public void ReceiveMessages()
+        {
+
+        }
+    }
+}
---
+++ LetsChatClient/Windows/ClientMainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LetsChatClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Client myClient;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConnectButton.Click += ConnectButton_Click;
+            SendMessageButton.Click += SendMessageButton_Click;
+        }
+
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            myClient = new Client();
+            myClient.Connect();
+            ConnectButton.IsEnabled = false;
+        }
+        private void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            myClient.SendMessage();
+        }
+
+    }
+}
